5ffa4e02102ab241df646258a87b91f955297196
AndreMwendwa <mwendwakiko@gmail.com>
2025-07-21 11:48:59 -0400

    Changes to __init__

--- src/__init__.py
+++ src/__init__.py
@@ -12,7 +12,19 @@ Main Components:
 - QueueDynamics: M/M/1 queue modeling
 - VisualizationManager: Plotting and analysis
 - MultiFloorAirportRestroomSimulator: Main orchestrator
+
+Additionally, some time series forecasting models are included:
+- ARIMAModel: Wrapper for statsmodels ARIMA
+- ProphetModel: Wrapper for Facebook Prophet
+- DayOfYearStatisticModel: Fallback option, a simple model that predicts based on historical statistics of the day of the year.
 """
+import sys
+import os
+current_file_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Add the directory to sys.path
+sys.path.append(current_file_dir)
+
 
 from config_manager import ConfigManager
 from flight_manager import FlightManager
@@ -21,6 +33,9 @@ from assignment_methods import AssignmentMethods
 from queue_dynamics import QueueDynamics
 from visualization import VisualizationManager
 from simulator import MultiFloorAirportRestroomSimulator
+from src.ts_arima import ARIMAModel
+from src.ts_day_of_year_statistic_model import DayOfYearStatisticModel
+from src.ts_prophet import ProphetModel
 
 __all__ = [
     'ConfigManager',
@@ -29,7 +44,10 @@ __all__ = [
     'AssignmentMethods',
     'QueueDynamics',
     'VisualizationManager',
-    'MultiFloorAirportRestroomSimulator'
+    'MultiFloorAirportRestroomSimulator',
+    'ARIMAModel',
+    'DayOfYearStatisticModel',
+    'ProphetModel'
 ]
 
 __version__ = "0.0.0"
